91e9fcfd5e0f31c50749d0e6e993f6f6a400b1d2
chaoen <jason831128@gmail.com>
2021-04-13 23:22:00 +0800

    can't exec

--- src/App.js
+++ src/App.js
@@ -1,22 +1,154 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+function ScrollingList(
+    error,
+    items,
+    isLoaded
+  ) {
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>姓名</th>
+            <th>職業</th>
+            <th>功能</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.job.name}</td>
+              <td><button onClick={()=>alert(item.id + "被點擊了")}>預約</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// class ScrollingListBak extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       error: null,
+//       items: [],
+//       isLoaded: false,
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch("http://127.0.0.1:7001/api/v1/human_resource")
+//       .then(res => res.json())
+//       .then(
+//         (result) => {
+//           this.setState({
+//             isLoaded: true,
+//             items: result.items
+//           });
+//         },
+//         (error) => {
+//           this.setState({
+//             isLoaded: true,
+//             error
+//           });
+//         }
+//       )
+//   }
+//   render() {
+//     const { error, isLoaded, items } = this.state;
+//     if (error) {
+//       return <div>Error: {error.message}</div>;
+//     } else if (!isLoaded) {
+//       return <div>Loading...</div>;
+//     } else {
+//       return (
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>姓名</th>
+//               <th>職業</th>
+//               <th>功能</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {items.map(item => (
+//               <tr key={item.id}>
+//                 <td>{item.name}</td>
+//                 <td>{item.job.name}</td>
+//                 <td><button onClick={()=>alert(item.id + "被點擊了")}>預約</button></td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       );
+//     }
+//   }
+// }
+
+function SearchBar(
+    name,
+    onChangeEvent
+  ) {
+  // const [name, setName] = useState('');
+  console.log(onChangeEvent)
+  return <div>
+      <label>姓名</label>
+      <input
+        placeholder="請輸入姓名"
+        value={name}
+        onChange={(e) => onChangeEvent(e.target)}
+      />
+    </div>;
+}
+
+// class SearchBarBak extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: '',
+//     }
+//   }
+
+//   render() {
+//     return <div>
+//         <label>姓名</label>
+//         <input type="text" value={this.state.name} />
+//       </div>;
+//   }
+// }
 
 function App() {
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleSearchNameChanged = function(target) {
+    setName(target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchBar
+          name={ name }
+          onChange={() => handleSearchNameChanged}
+        />
+        <ScrollingList
+          error={ error }
+          items={ items }
+          isLoaded={ isLoaded }
+        />
       </header>
     </div>
   );
